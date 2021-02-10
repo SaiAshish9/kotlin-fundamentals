446d9cbfb44755773433eacde3c465dd594d1230
saiashish9 <saiashish3760@gmail.com>
2021-02-10 21:11:54 +0530

    when clause  updated

---
+++ src/main/kotlin/conditional.kt
@@ -0,0 +1,36 @@
+fun main() {
+    val a = 5
+    val b = 10
+//    var maxValue:Int
+//    if(a>b)
+//        maxValue = a
+//    else {
+//        maxValue = b
+//    }
+//
+    var maxValue = if (a > b) {
+        println("a")
+        a
+    } else {
+        println("b")
+        b
+    }
+
+//    println(maxValue)
+
+    val x= 3
+    when(x){
+        1 -> println(1)
+        2 -> println(2)
+        else -> {
+            println("nf")
+        }
+    }
+
+//    if when expressions
+
+
+
+
+
+}
