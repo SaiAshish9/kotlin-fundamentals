63ef3859c77faa975a60c7af38f351e463a10d2d
saiashish9 <saiashish3760@gmail.com>
2021-02-12 00:52:13 +0530

    lambdas and closures updated

--- src/main/kotlin/lambdas.kt
+++ src/main/kotlin/lambdas.kt
@@ -1,5 +1,7 @@
 fun main(args:Array<String>) {
     val p = P()
+    var result = 0
+
     p.addTwoNumbers(1,2)
     p.addTwoNumbers(1,2,object: MyInterface{
         override fun execute(sum: Int) {
@@ -12,6 +14,21 @@ fun main(args:Array<String>) {
     }
     p.addTwoNumbers(2,7,myLambda)
 //    val myLambda = {s:Int => println(s)}
+
+//    High Level Functions
+    val myLambdaFunc: (Int,Int) -> Int = {x,y -> x+y}
+
+//    p.addTwoNumbers(3,4,myLambdaFunc)
+    p.addTwoNumbers(7,8){x,y -> x+y}
+
+    p.addTwoNumbers(7,8){
+        x,y-> {
+            result = x+y
+    }
+
+    }
+
+
 }
 
 class P{
@@ -29,7 +46,21 @@ class P{
         val sum = a+b
         println(sum)
     }
+    fun addTwoNumbers(a:Int,b:Int,myFunc:(Int,Int)->Unit){
+        println(myFunc(a,b))
+    }
+
+
+
 }
+
+// Closures
+
+// In java 8 , we cannot mutate(change) values of
+// outside variable inside Lambdas
+// But in Kotlin we can change the values
+
+
 
 interface MyInterface{
     fun execute(sum:Int)
