00c56ba9101905b9c139bc08b7eb513533ffc4f3
saiashish9 <saiashish3760@gmail.com>
2021-02-12 18:53:25 +0530

    kotlin highlights updated

---
+++ src/main/kotlin/KotlinHighlights.kt
@@ -0,0 +1,30 @@
+// Kotlin Highlights
+// In Google I/O 2007,
+// Kotlin is now official Android Language
+// Developed by JetBrains
+// JetBrains built Intellij IDEA
+// Android Studio is built on top of Intellij IDEA
+//
+// Kotlin is open source project primarily under Apache 2
+// which again promotes developer ecosystem to be
+// united and contribute more freely
+//
+//  Kotlin is just more flexible Java
+//  It is a more powerful language
+//  The Android apps can be built on Kotlin along with using Java and c++ alongside.
+//
+//  Kotlin is just more powerful java
+//  Expressive
+//  Concise
+//
+//  Safety Features
+//  Immutable
+//  Nullability: No more pull pointer exceptions
+//
+//  It's a JVM language
+//  It's syntax if more expressive and concise
+//  It supports:
+//  Higher order fns, lambda expressions, string templates,etc
+//
+//  Kotlin and java are interoperable
+//
